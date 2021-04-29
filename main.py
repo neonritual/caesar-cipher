afa402fa1ffe901656aede0f295111dae413ee53
V. S <>
2021-04-29 13:13:54 +0000

    fixed z error

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def Convert(string):
-    text_as_list=[]
-    text_as_list[:0]=string
-    return text_as_list
+# def Convert(string):
+#     text_as_list=[]
+#     text_as_list[:0]=string
+#     return text_as_list
 # print(Convert(text))
 
 
@@ -25,21 +25,34 @@ def Convert(string):
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-n = len(text)
-cipher_list = []
 
-def encrypt(text, shift):
-  x = 0
-  while x != n: 
-    index = alphabet.index(text[x])
-    outcode = alphabet[index + shift]
-    x += 1
-    cipher_list.append(outcode)
-  print(*cipher_list, sep='' )    
+#MY ORIGINAL TRY (works but not great)
+# n = len(text)
+# cipher_list = []
 
-  
-   
-encrypt(text, shift)
+# def encrypt(text, shift):
+#   x = 0
+#   while x != n: 
+#     index = alphabet.index(text[x])
+#     outcode = alphabet[index + shift]
+#     x += 1
+#     cipher_list.append(outcode)
+#   print(*cipher_list, sep='' ) 
+
+#TEACHER's VERSION for study
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    if position > 23:
+      position = -1
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    cipher_text += new_letter
+  print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
 
 # print(cipher_list)
 
